f63995a71b2e19a7b7411409d69d77a4f1acd888
TobiasRoeddiger <roeddiger@teco.edu>
2023-10-02 15:13:34 +0200

    added simple mutex to mitigate issues with multipel calls to gatt

--- assets/js/ConnectionManager.js
+++ assets/js/ConnectionManager.js
@@ -1,13 +1,12 @@
 var openEarable = new OpenEarable();
 
 openEarable.bleManager.subscribeOnConnected(async () => {
-    $('#connectDeviceButton').hide()
-    $('#disconnectDeviceButton').show()
-    $(".is-connect-enabled").prop('disabled', false);
 
     // Get device identifier and generation after connected
     const firmwareVersion = await openEarable.readFirmwareVersion();
     const hardwareVersion = await openEarable.readHardwareVersion();
+
+    $('#disconnectDeviceButton').prop('disabled', false);
 
     // Update the DOM with the obtained values
     $('#fwVersion').text(firmwareVersion);
@@ -21,29 +20,57 @@ openEarable.bleManager.subscribeOnConnected(async () => {
 openEarable.bleManager.subscribeOnDisconnected(() => {
     $('#disconnectDeviceButton').hide()
     $('#connectDeviceButton').show()
-    $(".is-connect-enabled").prop('disabled', true);
-    $('#batteryLevel').hide();
+    $("#connectDeviceButton").prop('disabled', false);
+    $('#batteryLevel').text("XX")
+    $('#batteryChargingIndicator').hide();
+    $('#batteryChargedIndicator').hide();
 
     log("OpenEarable disconnected.", type = "WARNING")
 
     // Reset the values to default when disconnected
-    $('#connectedDevice').text("OpenEarable not connected");
+    $('#connectedDevice').text("OpenEarable-XXXX");
     $('#fwVersion').text("X.X.X");
     $('#deviceVersion').text("X.X.X");
 });
 
 
 openEarable.subscribeBatteryLevelChanged((batteryLevel) => {
-    $('#batteryLevel').text(' (' + batteryLevel + '%)');
+    $('#connectDeviceButton').hide()
+    $('#disconnectDeviceButton').show()
+    $(".is-connect-enabled").prop('disabled', false);
+
+    $('#batteryLevel').text(batteryLevel);
     $('#batteryLevel').show();
 })
 
-$('#connectDeviceButton').click(() => {
+openEarable.subscribeBatteryStateChanged((batteryState) => {
+    if (batteryState === 1) {
+        $('#batteryChargingIndicator').show();
+        $('#batteryChargedIndicator').hide();
+    } else if (batteryState === 2) {
+        $('#batteryChargedIndicator').show();
+        $('#batteryChargingIndicator').hide();
+    } else {
+        $('#batteryChargingIndicator').hide();
+        $('#batteryChargedIndicator').hide();
+    }
+    
+})
+
+$('#connectDeviceButton').click(async () => {
+    $('#connectDeviceButton').prop('disabled', true);
     log("Scanning for OpenEarables. Please select.", type = "MESSAGE")
-    openEarable.bleManager.connect();
+    try {
+        await openEarable.bleManager.connect();
+    } catch (e) {
+        $('#connectDeviceButton').prop('disabled', false);
+    }
+    
 });
 
 $('#disconnectDeviceButton').click(() => {
+    $(".is-connect-enabled").prop('disabled', true);
+    $('#disconnectDeviceButton').prop('disabled', true);
     log("Disconnecting OpenEarable.", type = "MESSAGE")
     openEarable.bleManager.disconnect();
 });
--- assets/js/SensorManager.js
+++ assets/js/SensorManager.js
@@ -1,48 +1,48 @@
 $(document).ready(function () {
-    $('#setSensorConfigurationButton').on('click', function() {
+    $('#setSensorConfigurationButton').on('click', async function() {
         console.log("set sensor config")
         // Check if the checkbox for the first set of sensors is checked
         if ($('#areSensorsEnabled').is(':checked')) {
             var sensorSamplingRate = $('#sensorSamplingRate').val();
             log("Setting sampling rate for IMU: " + sensorSamplingRate + " Hz")
-            openEarable.sensorManager.writeSensorConfig(0, sensorSamplingRate, 0);
+            await openEarable.sensorManager.writeSensorConfig(0, sensorSamplingRate, 0);
         } else {
             // If the checkbox is not checked, set the sampling rate to 0
             log("Setting IMU disabled.")
-            openEarable.sensorManager.writeSensorConfig(0, 0, 0);
+            await openEarable.sensorManager.writeSensorConfig(0, 0, 0);
         }
 
         if ($('#isPressureSensorEnabled').is(':checked')) {
             var pressureSensorSamplingRate = $('#pressureSensorSamplingRate').val();
             log("Setting sampling rate for pressure sensor: " + pressureSensorSamplingRate + " Hz")
-            openEarable.sensorManager.writeSensorConfig(1, pressureSensorSamplingRate, 0);
+            await openEarable.sensorManager.writeSensorConfig(1, pressureSensorSamplingRate, 0);
         } else {
             log("Setting pressure sensor disabled.")
-            openEarable.sensorManager.writeSensorConfig(1, 0, 0);
+            await openEarable.sensorManager.writeSensorConfig(1, 0, 0);
         }
 
         // Check if the checkbox for the microphone is checked
         if ($('#isMicEnabled').is(':checked')) {
             var microphoneSamplingRate = $('#microphoneSamplingRate').val();
             log("Setting sampling rate for microphone: " + microphoneSamplingRate + " Hz")
-            openEarable.sensorManager.writeSensorConfig(2, microphoneSamplingRate, 0);
+            await openEarable.sensorManager.writeSensorConfig(2, microphoneSamplingRate, 0);
         } else {
             // If the checkbox is not checked, set the sampling rate to 0
             log("Setting microphone disabled.")
-            openEarable.sensorManager.writeSensorConfig(2, 0, 0);
+            await openEarable.sensorManager.writeSensorConfig(2, 0, 0);
         }
     });
 
-    $('.btn-disable-sensors').on('click', function() {
+    $('.btn-disable-sensors').on('click', async function() {
         // Set the sampling rate to 0 for all sensors
-        openEarable.sensorManager.writeSensorConfig(0, 0, 0);
-        openEarable.sensorManager.writeSensorConfig(1, 0, 0);
-        openEarable.sensorManager.writeSensorConfig(2, 0, 0);
+        await openEarable.sensorManager.writeSensorConfig(0, 0, 0);
+        await openEarable.sensorManager.writeSensorConfig(1, 0, 0);
+        await openEarable.sensorManager.writeSensorConfig(2, 0, 0);
 
         // Uncheck the checkboxes
-        $('#areSensorsEnabled, #isMicEnabled').prop('checked', false);
+        $('#areSensorsEnabled, #isMicEnabled, #isPressureSensorEnabled').prop('checked', false);
 
         // Reset the dropdowns to 0
-        $('#sensorSamplingRate, #microphoneSamplingRate').val('0');
+        $('#sensorSamplingRate, #microphoneSamplingRate, #pressureSensorSamplingRate').val('0');
     });
 });
